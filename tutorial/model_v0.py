6ef70e781782c27efaae4e3a8d8a4e46aba7dd4a
ztstar <zoutong2006@gmail.com>
2025-04-07 00:34:28 -0500

    tutorial basic setup

---
+++ tutorial/model_v0.py
@@ -0,0 +1,79 @@
+"""
+Two products: 
+RAINFOREST_RESIN: stable
+KELP: going up and down over time
+
+position limit: 50
+
+In v0, our simple strategy is: 
+keep track of the average operation price. 
+lower: buy
+higher: sell
+
+"""
+
+from typing import Dict, List
+from datamodel import *
+import jsonpickle
+
+class Trader:
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        # Initialize the method output dict as an empty dict
+        result = {}
+
+        traderData = state.traderData
+        # Iterate over all the keys (the available products) contained in the order dephts
+        for product in state.order_depths.keys():
+            
+            # Retrieve the Order Depth containing all the market BUY and SELL orders
+            order_depth: OrderDepth = state.order_depths[product]
+
+            # Initialize the list of Orders to be sent as an empty list
+            orders: list[Order] = []
+
+            # Note that this value of 1 is just a dummy value, you should likely change it!
+            acceptable_price = 10
+
+            # If statement checks if there are any SELL orders in the market
+            if len(order_depth.sell_orders) > 0:
+
+                # Sort all the available sell orders by their price,
+                # and select only the sell order with the lowest price
+                best_ask = min(order_depth.sell_orders.keys())
+                best_ask_volume = order_depth.sell_orders[best_ask]
+
+                    # Check if the lowest ask (sell order) is lower than the above defined fair value
+                if best_ask < acceptable_price:
+
+                    # In case the lowest ask is lower than our fair value,
+                    # This presents an opportunity for us to buy cheaply
+                    # The code below therefore sends a BUY order at the price level of the ask,
+                    # with the same quantity
+                    # We expect this order to trade with the sell order
+                    print("BUY", str(-best_ask_volume) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -best_ask_volume))
+
+            # The below code block is similar to the one above,
+            # the difference is that it find the highest bid (buy order)
+            # If the price of the order is higher than the fair value
+            # This is an opportunity to sell at a premium
+            if len(order_depth.buy_orders) != 0:
+                best_bid = max(order_depth.buy_orders.keys())
+                best_bid_volume = order_depth.buy_orders[best_bid]
+                if best_bid > acceptable_price:
+                    print("SELL", str(best_bid_volume) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -best_bid_volume))
+
+            # Add all the above the orders to the result dict
+            result[product] = orders
+                
+        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        
+        conversions = 1 
+
+        # Return the dict of orders
+        # These possibly contain buy or sell orders
+        # Depending on the logic above
+        
+        return result, conversions, traderData
